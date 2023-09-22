49b40c2ce8bc274f18abfcb5af5b92d016c8c6e1
TADOORI <vtadoori@ford.com>
2023-09-21 23:32:44 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!! Before this is too late and was changed!!!
         </a>
       </header>
     </div>
